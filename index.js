a9d7e923303e6ab77416f71b1a18f06275fb0e9e
Lana Holmes <lana.lee.holmes@gmail.com>
2018-11-25 01:21:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -57,5 +57,12 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  var finalTotal = total()
+  if (cardNumber === undefined) {
+    return ("Sorry, we don't have a credit card on file for you.");
+  }
+  else {
+    cart.splice(0, cart.length);
+    return ("Your total cost is $" + {finalTotal} + `which will be charged to the card ${cardNumber}.`)
+  }
 }
