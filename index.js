f801479868076163a6d4eac83b6ebaa40e1215ff
Lana Holmes <lana.lee.holmes@gmail.com>
2018-11-25 01:43:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -48,11 +48,11 @@ function total() {
 }
 
 function removeFromCart(item) {
-  for (var i = 0; i === cart.length; i++) {
+  for (var i = 0; i < cart.length; i++) {
     if (cart[i].itemName === item) {
       return cart.splice(i, 1);
     }
-    else if (i === cart.length) {
+    else {
       return ("That item is not in your cart.");
     }
   }
