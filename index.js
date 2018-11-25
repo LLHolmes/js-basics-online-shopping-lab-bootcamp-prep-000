7d5785f052cde825050472ca49e3718d97d93bb0
Lana Holmes <lana.lee.holmes@gmail.com>
2018-11-25 00:42:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,11 +10,34 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+ var cost = x => Math.floor(Math.random() * 100) + 1
+ var newItem = {
+   itemName : item,
+   itemPrice : cost()
+ }
+ cart.push(newItem);
+ return item + " has been added to your cart."
 }
 
 function viewCart() {
-  // write your code here
+  var list = ("In your cart, you have");
+  if (cart.length === 0) {
+    return ("Your shopping cart is empty.");
+  }
+  else if (cart.length === 1) {
+    return (list + (` ${cart[0].itemName} at $` + cart[0].itemPrice + "."));
+  }
+  else {
+    for (var i = 0; i < cart.length; i++) {
+      if (i === (cart.length - 1)) {
+        list = (list + (` and ${cart[i].itemName} at $` + cart[i].itemPrice + "."));
+        return list;
+      }
+      else {
+        list = (list + ` ${cart[i].itemName} at $` + cart[i].itemPrice + ",");
+      }
+    }
+  }
 }
 
 function total() {
